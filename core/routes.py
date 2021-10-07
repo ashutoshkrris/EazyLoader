54c0c33fc2f9bbfe654fab02f2f631853769274e
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-10-07 20:32:38 +0530

    Remove Sleep Time

--- core/routes.py
+++ core/routes.py
@@ -147,7 +147,6 @@ def ig_video_downloader():
             driver.get(url)
             print("Page opened")
             print(driver.current_url)
-            time.sleep(10)
             if 'login' in driver.current_url:
                 driver.find_element_by_name(
                     'username').send_keys(IG_USERNAME)
