b7955def91e4e784b1c0aadccdb5d7d2e60ebf0a
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-10-07 19:24:41 +0530

    Add Insta Login

--- core/routes.py
+++ core/routes.py
@@ -24,7 +24,7 @@ IG_PASSWORD = config('IG_PASSWORD')
 chrome_options = webdriver.ChromeOptions()
 user_agent = 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Mobile Safari/537.36'
 chrome_options.add_argument(f'user-agent={user_agent}')
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 # chrome_options.add_argument('--incognito')
 
 if not app.debug:
