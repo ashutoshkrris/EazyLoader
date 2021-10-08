bd4c87786b1500cc3522237e630f99db525745f7
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-10-08 08:34:55 +0530

    Fix IG Username and Password

--- core/routes.py
+++ core/routes.py
@@ -17,14 +17,14 @@ from selenium.webdriver.common.keys import Keys
 import time
 from core.utils import playlist
 
-IG_USERNAME = config('IG_USERNAME')
-IG_PASSWORD = config('IG_PASSWORD')
+IG_USERNAME = config('IG_USERNAME', default='username')
+IG_PASSWORD = config('IG_PASSWORD', default='password')
 
 
 chrome_options = webdriver.ChromeOptions()
 user_agent = 'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Mobile Safari/537.36'
 chrome_options.add_argument(f'user-agent={user_agent}')
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 # chrome_options.add_argument('--incognito')
 
 if not app.debug:
