291719fbe003aeb4ae4c4a84ec48a80f39a14cb1
Daksh Kanotra <59220834+daksh10110@users.noreply.github.com>
2021-10-17 18:17:39 +0530

    Minor fixes

--- core/utils/ig_image_downloader.py
+++ core/utils/ig_image_downloader.py
@@ -4,15 +4,16 @@ from selenium import webdriver
 import os
 
 class IgImage:
-    def __init__(self, path, link, chrome_driver_path):
+    def __init__(self, path, link, chrome_driver_path, chrome_path):
         self.link = link
         self.chromedriver = chrome_driver_path
+        self.chrome = chrome_path
         self.filepath = os.path.join(path, "ig_image.png")
 
     def save_image(self):
         try:
             chrome_options = webdriver.ChromeOptions()
-            chrome_options.binary_location = os.environ.get("/lib/chromium/chromium")
+            chrome_options.binary_location = os.environ.get(self.chrome)
             chrome_options.add_argument("--headless")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--no-sandbox")
@@ -30,6 +31,8 @@ class IgImage:
                 f.write(r.content)
 
             print(self.filepath)
+            
+            return "Image is downloaded"
         except:
             return "Try again Later"
 
