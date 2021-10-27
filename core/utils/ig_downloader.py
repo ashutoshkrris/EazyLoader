3819275bce2a6cb8b0f22b1720e9b6aeb9d751e6
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-10-27 10:17:26 +0530

    Add IG Stories Downloader

--- core/utils/ig_downloader.py
+++ core/utils/ig_downloader.py
@@ -2,13 +2,15 @@ import os
 from instaloader import Instaloader, Post, Profile
 from zipfile import ZipFile
 
-
 class IGDownloader:
     """IGDownloader Class - takes login username and password as argument"""
 
     def __init__(self, login_username, login_password):
         self.loader = Instaloader()
         self.loader.login(login_username, login_password)
+        self.loader.post_metadata_txt_pattern = ''
+        self.loader.save_metadata = False
+        self.loader.download_comments = False
 
     def download_profile_picture(self, username: str) -> str:
         """Download profile picture for any Instagram profile
@@ -19,9 +21,6 @@ class IGDownloader:
         """
         self.loader.download_pictures = True
         self.loader.download_videos = False
-        self.loader.post_metadata_txt_pattern = ''
-        self.loader.save_metadata = False
-        self.loader.download_comments = False
 
         self.profile = Profile.from_username(self.loader.context, username)
         self.loader.download_profilepic(self.profile)
@@ -37,9 +36,6 @@ class IGDownloader:
         """
         self.loader.download_pictures = True
         self.loader.download_videos = False
-        self.loader.post_metadata_txt_pattern = ''
-        self.loader.save_metadata = False
-        self.loader.download_comments = False
     
         post_id = post_url.split('/')[4]
         try:
@@ -88,6 +84,33 @@ class IGDownloader:
 
             self.loader.download_post(post, video_id)
             return video_id
+        except Exception as e:
+            print(e)
+            return None
+
+    def download_latest_stories(self, username: str):
+        """Download post image from public accounts
+
+        Keyword arguments:
+        username -- str
+        Return: zip filename or None
+        """
+
+        profile_id = self.loader.check_profile_id(username).userid
+        try:
+            self.loader.download_stories(userids=[profile_id], filename_target=username)
+            
+            zfname = f'{username}.zip'
+            foo = ZipFile(zfname, 'w')
+           # Adding files from directory 'post_id'
+            for root, dirs, files in os.walk(username):
+                for f in files:   
+                    if not f.lower().startswith(('id')): 
+                        foo.write(os.path.join(root, f))
+                    os.remove(os.path.join(root, f))
+            foo.close()
+            os.removedirs(username)
+            return zfname
         except Exception as e:
             print(e)
             return None
