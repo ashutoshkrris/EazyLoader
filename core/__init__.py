39b20aea902d48c200b3df7c7fc8fb22cc9c7d83
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-11-12 22:40:10 +0530

    Revert Commit to dd219eb

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from decouple import config
 from core.utils.ig_downloader import IGDownloader
 from logging.config import dictConfig
-from core.utils.slideshare_downloader import SlideShareDownloader
 from core.utils.yt_downloader import YTDownloader
 from flask_mail import Mail
 from flask_socketio import SocketIO
@@ -16,14 +15,11 @@ app.config.from_object(config("APP_SETTINGS"))
 print("Logging into IG Account")
 ig = IGDownloader(IG_USERNAME, IG_PASSWORD)
 yt = YTDownloader()
-ss = SlideShareDownloader()
-
-status = {}
 
 mail = Mail(app)
 socketio = SocketIO(app)
 
 app.jinja_env.globals['momentjs'] = momentjs
 
-from core.routes import core, instagram, youtube, slideshare, socket
+from core.routes import core, instagram, youtube
 from core.utils import custom_filters, playlist, contributors
