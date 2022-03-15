0de876252ee8f39416e093ddf9b1fd474955b6b7
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-03-15 11:30:04 +0530

    Update blogs.py

--- core/utils/blogs.py
+++ core/utils/blogs.py
@@ -2,10 +2,10 @@ import requests
 
 
 def fetch_posts():
-    response = requests.get("https://iread.ga/api/v1/eazyloader/posts")
+    response = requests.get("https://ireadblog.com/api/v1/eazyloader/posts")
     return response.json()
 
 
 def get_blog_post(id, slug):
-    response = requests.get(f"https://iread.ga/api/v1/post/{id}/{slug}")
+    response = requests.get(f"https://ireadblog.com/api/v1/post/{id}/{slug}")
     return response.json()[0]
