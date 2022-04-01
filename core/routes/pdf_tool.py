9d60e05cec30b3a52f954f1851034a838ec8d1fd
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-04-01 19:39:45 +0530

    Add Decrypt Password

--- core/routes/pdf_tool.py
+++ core/routes/pdf_tool.py
@@ -2,7 +2,7 @@ from core import app
 from flask import render_template, request, flash, send_from_directory, redirect
 import os
 from werkzeug.utils import secure_filename
-from core.utils.pdf_tools import encrypt_file
+from core.utils.pdf_tools import decrypt_file, encrypt_file
 
 ALLOWED_EXTENSIONS = {'pdf'}
 
@@ -20,12 +20,14 @@ def encrypt_pdf_page():
             return redirect(request.url)
         pdf_file = request.files['pdf-file']
         password = request.form.get('password')
+        if password and len(password) < 5:
+            flash('Password is too short, please choose a strong password', 'error')
+            return redirect(request.url)
 
         if pdf_file and allowed_file(pdf_file.filename):
             filename = secure_filename(pdf_file.filename)
             saved_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            output_path = str(saved_path).removesuffix(
-                '.pdf') + '_encrypted.pdf'
+            output_path = saved_path.removesuffix('.pdf') + '_encrypted.pdf'
             output_name = filename.removesuffix('.pdf') + '_encrypted.pdf'
             pdf_file.save(saved_path)
             result = encrypt_file(saved_path, password, output_path)
@@ -44,4 +46,30 @@ def encrypt_pdf_page():
 
 @app.route("/pdf-tools/decrypt", methods=["GET", "POST"])
 def decrypt_pdf_page():
+    if request.method == "POST":
+        if not 'pdf-file' in request.files:
+            flash('PDF file is missing', 'error')
+            return redirect(request.url)
+        if not 'password' in request.form:
+            flash('Password is missing', 'error')
+            return redirect(request.url)
+        pdf_file = request.files['pdf-file']
+        password = request.form.get('password')
+
+        if pdf_file and allowed_file(pdf_file.filename):
+            filename = secure_filename(pdf_file.filename)
+            saved_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            output_path = saved_path.removesuffix('.pdf') + '_decrypted.pdf'
+            output_name = filename.removesuffix('.pdf') + '_decrypted.pdf'
+            pdf_file.save(saved_path)
+            result = decrypt_file(saved_path, password, output_path)
+            if result == 1:
+                return send_from_directory(app.config['UPLOAD_FOLDER'], output_name, as_attachment=True)
+            elif result == 2:
+                flash(
+                    'Your file is already decrypted. Feel free to encrypt it using our tool', 'error')
+                return redirect(request.url)
+            elif result == 3:
+                flash('Error while reading your file, please check your password', 'error')
+                return redirect(request.url)
     return render_template('pdf-tools/decrypt.html', title="Decrypt PDF")
--- core/utils/pdf_tools.py
+++ core/utils/pdf_tools.py
@@ -9,7 +9,7 @@ def is_encrypted(filepath: str) -> bool:
 
 # Responses:
 # 1 -> Encryption Success
-# 2 -> Already encrypted
+# 2 -> Already encrypted/decrypted
 # 3 -> Error while reading file
 
 
@@ -18,14 +18,14 @@ def encrypt_file(filepath: str, password: str, output_path: str) -> int:
     pdf_reader = PdfFileReader(open(filepath, 'rb'), strict=False)
     if is_encrypted(filepath):
         return 2
-    
+
     try:
         for page_number in range(pdf_reader.numPages):
             pdf_writer.addPage(pdf_reader.getPage(page_number))
         if not password:
             password = 'EazyLoader'
         pdf_writer.encrypt(user_pwd=password, use_128bit=True)
-        
+
         with open(output_path, "wb") as f:
             pdf_writer.write(f)
         return 1
@@ -33,21 +33,19 @@ def encrypt_file(filepath: str, password: str, output_path: str) -> int:
         return 3
 
 
-
-def decrypt_file(filename: str, password: str) -> str:
+def decrypt_file(filepath: str, password: str, output_path: str) -> int:
     pdf_writer = PdfFileWriter()
-    pdf_reader = PdfFileReader(open(filename, 'rb'), strict=False)
-    if not is_encrypted(filename):
-        return "PDF File is not encrypted."
+    pdf_reader = PdfFileReader(open(filepath, 'rb'), strict=False)
+    if not is_encrypted(filepath):
+        return 2
 
     pdf_reader.decrypt(password=password)
     try:
         for page_number in range(pdf_reader.numPages):
             pdf_writer.addPage(pdf_reader.getPage(page_number))
+        with open(output_path, "wb") as f:
+            pdf_writer.write(f)
+
+        return 1
     except utils.PdfReadError:
-        return "Error while reading PDF file"
-
-    with open("decypted_demo.pdf", "wb") as f:
-        pdf_writer.write(f)
-
-    return "PDF file decrypted successfully"
+        return 3
